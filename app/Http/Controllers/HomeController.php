093833a3bb20a99d87908eda44ec58541fef65c9
slot013 <slot00013@gmail.com>
2025-05-31 18:17:17 +0300

    fix0.3-изменил время кеширования

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,11 +14,11 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $featuredPosts = Cache::remember('featuredPosts', now()->addDay(), function () {
+        $featuredPosts = Cache::remember('featuredPosts', now()->addMinutes(3), function () {
             return Post::published()->featured()->with('categories')->latest('published_at')->take(3)->get();
         });
 
-        $latestPosts = Cache::remember('latestPosts', now()->addDay(), function () {
+        $latestPosts = Cache::remember('latestPosts', now()->addMinutes(3), function () {
             return Post::published()->with('categories')->latest('published_at')->take(9)->get();
         });
 
